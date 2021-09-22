f783856279401966ea5d3bddd7d00dbc42821bb2
katsumi-watanabe <ktm981124@gmail.com>
2021-09-22 18:24:07 +0900

    ~

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ public class Main {
 
   public static void main(String[] args) {
     // 3-9
-    for (int i = 1; i < 10; i++) {
-      for (int j = 1; j < 10; j++) {
-        System.out.println(i * j); // 掛け算の結果を出力
-        System.out.println(" "); // 空白を出力
-      }
-      System.out.println(""); // 改行を出力
-    }
+    // for (int i = 1; i < 10; i++) {
+    // for (int j = 1; j < 10; j++) {
+    // System.out.println(i * j); // 掛け算の結果を出力
+    // System.out.println(" "); // 空白を出力
+    // }
+    // System.out.println(""); // 改行を出力
+    // }
 
     // 3-8
     // for (int i = 0; i < 3; i++) {
