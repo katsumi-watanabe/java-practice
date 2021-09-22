a7adfa9eb82906aad12f881efe195e055b6cb0ec
katsumi-watanabe <ktm981124@gmail.com>
2021-09-22 10:54:00 +0900

    条件分岐

--- Main.java
+++ Main.java
@@ -1,13 +1,52 @@
-import java.util.Scanner;
 
 public class Main {
-  private static final Scanner SCANNER = new java.util.Scanner(System.in);
+  // private static final Scanner SCANNER = new java.util.Scanner(System.in);
 
   public static void main(String[] args) {
-    // final double PI = 3.14;
-    // int pie = 5;
-    // System.out.println("半径" + pie + "cmのパイの面積は、");
-    // System.out.println(pie * pie * PI);
+    // 3-7
+    for (int i = 0; i < 10; i++) {
+      System.out.println("こんにちは");
+    }
+
+    // 3-6
+    // System.out.println("あなたの今日の運勢を占います");
+    // int fortune = new java.util.Random().nextInt(5) + 1;
+    // switch (fortune) {
+    // case 1:
+    // case 2:
+    // System.out.println("いいね");
+    // break;
+    // case 3:
+    // System.out.println("普通です");
+    // break;
+    // case 4:
+    // case 5:
+    // System.out.println("うーん・・・");
+    // }
+
+    // boolean doorClose = false;
+    // while (doorClose == true) {
+    // System.out.println("ノックする");
+    // System.out.println("1分待つ");
+    // }
+
+    // boolean tenki = true;
+    // if (tenki == true) {
+    // System.out.println("洗濯をします");
+    // System.out.println("散歩にいきます");
+    // } else {
+    // System.out.println("DVDを見ます");
+    // }
+
+    // 2-15
+    // System.out.println("あなたの名前を入力してください。");
+    // String name = SCANNER.nextLine();
+    // System.out.println("あなたの年齢を入力してください。");
+    // int age = SCANNER.nextInt();
+    // System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+    // 2-14
+    // int r = new java.util.Random().nextInt(90);
+    // System.out.println("あなたは多分" + r + "歳ですよね？");
 
     // 2-5
     // float f = 3;
@@ -15,15 +54,9 @@ public class Main {
     // System.out.println(f);
     // System.out.println(d);
 
-    // 2-14
-    // int r = new java.util.Random().nextInt(90);
-    // System.out.println("あなたは多分" + r + "歳ですよね？");
-
-    // 2-15
-    System.out.println("あなたの名前を入力してください。");
-    String name = SCANNER.nextLine();
-    System.out.println("あなたの年齢を入力してください。");
-    int age = SCANNER.nextInt();
-    System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+    // final double PI = 3.14;
+    // int pie = 5;
+    // System.out.println("半径" + pie + "cmのパイの面積は、");
+    // System.out.println(pie * pie * PI);
   }
 }
