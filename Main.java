73342ce604e0cdc63dc3f8bdd82027f03e949394
katsumi-watanabe <ktm981124@gmail.com>
2021-09-23 17:17:33 +0900

    4−11

--- Main.java
+++ Main.java
@@ -3,15 +3,25 @@ public class Main {
   // private static final Scanner SCANNER = new java.util.Scanner(System.in);
 
   public static void main(String[] args) {
-    // 4-10
+    // 4-11
     int[] scores = { 20, 30, 40, 50, 80 };
-    int sum = 0;
+    int count = 0;
     for (int i = 0; i < scores.length; i++) {
-      sum += scores[i];
+      if (scores[i] >= 50) {
+        count++;
+      }
     }
-    int avg = sum / scores.length;
-    System.out.println("合計点：" + sum);
-    System.out.println("平均点：" + avg);
+    System.out.println("50点以上の科目の数は：" + count);
+
+    // 4-10
+    // int[] scores = { 20, 30, 40, 50, 80 };
+    // int sum = 0;
+    // for (int i = 0; i < scores.length; i++) {
+    // sum += scores[i];
+    // }
+    // int avg = sum / scores.length;
+    // System.out.println("合計点：" + sum);
+    // System.out.println("平均点：" + avg);
 
     // 4-9
     // int[] scores = { 20, 30, 40, 50, 80 };
