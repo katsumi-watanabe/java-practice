ab6a0c512738fec973b8423cd1dba7812cfa3fc5
katsumi-watanabe <ktm981124@gmail.com>
2021-09-29 00:34:44 +0900

    Practice6

---
+++ Practice/Practice6/Main.java
@@ -0,0 +1,18 @@
+package practice.Practice6;
+
+import practice.Practice6.comment.Zenhan;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    practice.Practice6.comment.Kouhan.callDeae();
+    practice.Practice6.comment.Kouhan.showMondokoro();
+  }
+
+  // 6-4
+  // Main.class → c:¥work¥ex64 フォルダ
+  // Koumon.class →c:¥work¥ex64¥comment フォルダ
+  // 6-5
+  // → c:¥javaapp¥koumon
+}
