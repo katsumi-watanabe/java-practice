e94d283ea79944cb21df77b66bddfde0671c2237
katsumi-watanabe <ktm981124@gmail.com>
2021-10-03 18:56:02 +0900

    9-6

--- Main9/Main.java
+++ Main9/Main.java
@@ -2,12 +2,27 @@ package Main9;
 
 public class Main {
   public static void main(String[] args) {
-    Hero h1;
-    h1 = new Hero();
+    // Sword s = new Sword();
+    // s.name = "炎の剣";
+    // s.damage = 10;
+    // Hero h = new Hero();
+    // h.name = "ミナト";
+    // h.hp = 100;
+    // h.sword = s;
+    // System.out.println("現在の武器は" + h.sword.name);
+
+    // 9-6
+    Hero h1 = new Hero();
+    h1.name = "ミナト";
     h1.hp = 100;
-    Hero h2;
-    h2 = h1;
-    h2.hp = 200;
-    System.out.println(h1.hp);
+    Hero h2 = new Hero();
+    h2.name = "アカサ";
+    h2.hp = 100;
+    Wizard w = new Wizard();
+    w.name = "スガワラ";
+    w.hp = 50;
+    w.heal(h1); // ミナトを回復させる（HP：100⇨110）
+    w.heal(h2); // アサカを回復させる（HP：100⇨110）
+    w.heal(h2); // アサカを回復させる（HP：110⇨120）
   }
 }
