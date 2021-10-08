51a496ac2850350465944cf585dfd89589a4c23a
katsumi-watanabe <ktm981124@gmail.com>
2021-10-08 12:23:45 +0900

    8-13

--- Mains8/Main.java
+++ Mains8/Main.java
@@ -3,6 +3,13 @@ package Mains8;
 public class Main {
   // 8-1
   public static void main(String[] args) {
-
+    Hero h = new Hero();
+    h.name = "湊";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました！");
+    h.sit(4);
+    h.slip();
+    h.sit(223);
+    h.run();
   }
 }
