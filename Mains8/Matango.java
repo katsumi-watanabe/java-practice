b05e97c588bf5e1a5c70d4f9c3037ebc026ea23b
katsumi-watanabe <ktm981124@gmail.com>
2021-10-09 17:15:55 +0900

    8-12

--- Mains8/Matango.java
+++ Mains8/Matango.java
@@ -3,4 +3,9 @@ package Mains8;
 public class Matango {
   int hp;
   final int LEVEL = 10;
+  char suffix;
+
+  public void run() {
+    System.out.println("お化けキノコ" + this.suffix + "は、逃げ出した！");
+  }
 }
