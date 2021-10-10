bf89060ffee18570470650b807170c2e5d26afb0
katsumi-watanabe <ktm981124@gmail.com>
2021-10-10 21:20:48 +0900

    9-7

--- Mains9/Main.java
+++ Mains9/Main.java
@@ -29,18 +29,22 @@ public class Main {
     // System.out.println("現在の武器は" + h.sword.name);
 
     // 9-6
-    Hero h1;
-    h1 = new Hero();
-    h1.name = "ミナト";
-    h1.hp = 100;
-    Hero h2 = new Hero();
-    h2.name = "アサカ";
-    h2.hp = 100;
-    Wizard w = new Wizard();
-    w.name = "スガワラ";
-    w.hp = 50;
-    w.heal(h1);
-    w.heal(h2);
-    w.heal(h2);
+    // Hero h1;
+    // h1 = new Hero();
+    // h1.name = "ミナト";
+    // h1.hp = 100;
+    // Hero h2 = new Hero();
+    // h2.name = "アサカ";
+    // h2.hp = 100;
+    // Wizard w = new Wizard();
+    // w.name = "スガワラ";
+    // w.hp = 50;
+    // w.heal(h1);
+    // w.heal(h2);
+    // w.heal(h2);
+
+    // 9-7
+    String s = new String("こんにちは");
+    System.out.println(s);
   }
 }
