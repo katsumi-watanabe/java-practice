104e279ebe3a71a2c54e4b59db3a2a2042b44aa8
katsumi-watanabe <ktm981124@gmail.com>
2021-10-13 23:29:05 +0900

    10-1

--- Mains10/Hero.java
+++ Mains10/Hero.java
@@ -3,4 +3,14 @@ package Mains10;
 public class Hero {
   // 10=1
   String name = "ミナト";
+  int hp = 100;
+
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃！");
+    m.hp -= 5;
+    System.out.println("5ポイントのダメージをあたえた！");
+    public void run(){
+      System.out.println(this.name+"は逃げ出した！");
+    }
+  }
 }
